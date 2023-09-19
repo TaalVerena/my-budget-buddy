e94177b60a90f7cb2e70ea9928eff88b5b838e84
S Bester <sarahleebester@gmail.com>
2023-09-19 06:40:34 +0100

    feat(script): Add displayModal function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
+// Income & budget modal
+let goButton = document.getElementById('go-button');
 
+goButton.addEventListener('click', function () {
+    let incomeBudgetModal = document.getElementById('income-budget-modal');
+    displayModal(incomeBudgetModal);
+});
+
+function displayModal(modalName) {
+    modalName.showModal();
+}
