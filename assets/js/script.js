7b184df71a64a313dea3f7dc00724d08ef747129
S Bester <sarahleebester@gmail.com>
2023-09-21 20:37:25 +0100

    feat(script): Adjust pay period event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,63 +8,63 @@ let resultsModal = document.getElementById('results-modal');
 let resultsBackButton = document.getElementById('results-back-button');
 
 let payPeriodDropdown = document.getElementById('pay-period');
-let incomeTitle = document.getElementById('income-title');
-let budgetTitle = document.getElementById('budget-title');
-let spendsTitle = document.getElementById('spends-title');
-let resultsTitle = document.getElementById('results-title');
 
 // Display income & budget modal
-document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener('DOMContentLoaded', function () {
     let goButton = document.getElementById('go-button');
 
-    goButton.addEventListener('click', function (){
+    goButton.addEventListener('click', function () {
         displayModal(incomeBudgetModal);
     });
 })
 
 // Close income & budget modal
-incomeBudgetBackButton.addEventListener('click', function (){
+incomeBudgetBackButton.addEventListener('click', function () {
     closeModal(incomeBudgetModal);
 });
 
 // Display spends modal
-incomeBudgetNextButton.addEventListener('click', function(){
+incomeBudgetNextButton.addEventListener('click', function () {
     displayModal(spendsModal);
 })
 
 // Close spends modal
-spendsBackButton.addEventListener('click', function (){
+spendsBackButton.addEventListener('click', function () {
     closeModal(spendsModal);
 });
 
 // Display results modal
-spendsNextButton.addEventListener('click', function(){
+spendsNextButton.addEventListener('click', function () {
     displayModal(resultsModal);
 })
 
 // Close results modal
-resultsBackButton.addEventListener('click', function (){
+resultsBackButton.addEventListener('click', function () {
     closeModal(resultsModal);
 });
 
 /**
  * Shows / displays the model passed as the parameter
  */
-function displayModal(modalName){
+function displayModal(modalName) {
     modalName.showModal();
 }
 
 /**
  * Closes the model passed as the parameter
  */
-function closeModal(modalName){
+function closeModal(modalName) {
     modalName.close();
 }
 
 // Listen for changes in the pay period dropdown
-payPeriodDropdown.addEventListener('change', function() {
+payPeriodDropdown.addEventListener('change', function () {
     let selectedPayPeriod = payPeriodDropdown.value;
-    
+    let incomeTitle = document.getElementById('income-title');
+    let budgetTitle = document.getElementById('budget-title');
+    let spendsTitle = document.getElementById('spends-title');
+    let resultsTitle = document.getElementById('results-title');
+
     // Update the modal titles based on the selected pay period
     if (selectedPayPeriod === 'weekly') {
         incomeTitle.textContent = 'My Weekly Income';
