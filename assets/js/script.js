ce0c39e3eaee472acb5dee0e243a19b033f9d66b
S Bester <sarahleebester@gmail.com>
2023-09-21 08:48:29 +0100

    feat(script): Add event listener for back button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ let incomeBudgetModal = document.getElementById('income-budget-modal');
 let incomeBudgetBackButton = document.getElementById('income-budget-back-button');
 let incomeBudgetNextButton = document.getElementById('income-budget-next-button');
 let spendsModal = document.getElementById('spends-modal');
+let spendsBackButton = document.getElementById('spends-back-button');
+let spendsNextButton = document.getElementById('spends-next-button');
 
 // Display income & budget modal
 document.addEventListener('DOMContentLoaded', function (){
@@ -21,6 +23,11 @@ incomeBudgetBackButton.addEventListener('click', function (){
 incomeBudgetNextButton.addEventListener('click', function(){
     displayModal(spendsModal);
 })
+
+// Close spends modal
+spendsBackButton.addEventListener('click', function (){
+    closeModal(spendsModal);
+});
 
 /**
  * Shows / displays the model passed as the parameter
