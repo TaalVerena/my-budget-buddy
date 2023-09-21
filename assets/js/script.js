8151367788146396b5307a670be15f858e5349a0
S Bester <sarahleebester@gmail.com>
2023-09-21 21:27:24 +0100

    feat(script): Adjust & remove global variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,87 @@
-let incomeBudgetModal = document.getElementById('income-budget-modal');
-let incomeBudgetBackButton = document.getElementById('income-budget-back-button');
-let incomeBudgetNextButton = document.getElementById('income-budget-next-button');
-let spendsModal = document.getElementById('spends-modal');
-let spendsBackButton = document.getElementById('spends-back-button');
-let spendsNextButton = document.getElementById('spends-next-button');
-let resultsModal = document.getElementById('results-modal');
-let resultsBackButton = document.getElementById('results-back-button');
-
-let payPeriodDropdown = document.getElementById('pay-period');
-
-let currency = document.getElementById('currency');
-
 // Display income & budget modal
 document.addEventListener('DOMContentLoaded', function () {
-    let goButton = document.getElementById('go-button');
+    const goButton = document.getElementById('go-button');
+    const incomeBudgetModal = document.getElementById('income-budget-modal');
 
     goButton.addEventListener('click', function () {
         displayModal(incomeBudgetModal);
     });
-})
-
-// Close income & budget modal
-incomeBudgetBackButton.addEventListener('click', function () {
-    closeModal(incomeBudgetModal);
-});
-
-// Display spends modal
-incomeBudgetNextButton.addEventListener('click', function () {
-    displayModal(spendsModal);
-})
-
-// Close spends modal
-spendsBackButton.addEventListener('click', function () {
-    closeModal(spendsModal);
-});
-
-// Display results modal
-spendsNextButton.addEventListener('click', function () {
-    displayModal(resultsModal);
-})
-
-// Close results modal
-resultsBackButton.addEventListener('click', function () {
-    closeModal(resultsModal);
 });
 
 /**
- * Shows / displays the model passed as the parameter
+ * Shows / displays the modal passed as the parameter
  */
 function displayModal(modalName) {
     modalName.showModal();
 }
 
 /**
- * Closes the model passed as the parameter
+ * Closes the modal passed as the parameter
  */
 function closeModal(modalName) {
     modalName.close();
 }
 
-// Listen for changes in the pay period dropdown
-payPeriodDropdown.addEventListener('change', function () {
-    let selectedPayPeriod = payPeriodDropdown.value;
-    let incomeTitle = document.getElementById('income-title');
-    let budgetTitle = document.getElementById('budget-title');
-    let spendsTitle = document.getElementById('spends-title');
-    let resultsTitle = document.getElementById('results-title');
-
-    // Update the modal titles based on the selected pay period
-    if (selectedPayPeriod === 'weekly') {
-        incomeTitle.textContent = 'My Weekly Income';
-        budgetTitle.textContent = 'My Weekly Budget';
-        spendsTitle.textContent = 'My Weekly Spends';
-        resultsTitle.textContent = 'My Weekly Results';
-    } else if (selectedPayPeriod === 'fortnightly') {
-        incomeTitle.textContent = 'My Fortnightly Income';
-        budgetTitle.textContent = 'My Fortnightly Budget';
-        spendsTitle.textContent = 'My Fortnightly Spends';
-        resultsTitle.textContent = 'My Fortnightly Results';
-    } else if (selectedPayPeriod === 'monthly') {
-        incomeTitle.textContent = 'My Monthly Income';
-        budgetTitle.textContent = 'My Monthly Budget';
-        spendsTitle.textContent = 'My Monthly Spends';
-        resultsTitle.textContent = 'My Monthly Results';
-    }
+// Close income & budget modal
+document.getElementById('income-budget-back-button').addEventListener('click', function () {
+    closeModal(document.getElementById('income-budget-modal'));
 });
 
+// Display spends modal
+document.getElementById('income-budget-next-button').addEventListener('click', function () {
+    displayModal(document.getElementById('spends-modal'));
+});
+
+// Close spends modal
+document.getElementById('spends-back-button').addEventListener('click', function () {
+    closeModal(document.getElementById('spends-modal'));
+});
+
+// Display results modal
+document.getElementById('spends-next-button').addEventListener('click', function () {
+    displayModal(document.getElementById('results-modal'));
+});
+
+// Close results modal
+document.getElementById('results-back-button').addEventListener('click', function () {
+    closeModal(document.getElementById('results-modal'));
+});
+
+// Listen for a change in the pay period & call the updateModalTitles function
+document.getElementById('pay-period').addEventListener('change', function () {
+    const selectedPayPeriod = this.value;
+    updateModalTitles(selectedPayPeriod);
+});
+
+/**
+ * Update modal titles based on pay period selected
+ */
+function updateModalTitles(selectedPayPeriod) {
+    const incomeTitle = document.getElementById('income-title');
+    const budgetTitle = document.getElementById('budget-title');
+    const spendsTitle = document.getElementById('spends-title');
+    const resultsTitle = document.getElementById('results-title');
+
+    let titlePrefix = '';
+
+    if (selectedPayPeriod === 'weekly') {
+        titlePrefix = 'My Weekly ';
+    } else if (selectedPayPeriod === 'fortnightly') {
+        titlePrefix = 'My Fortnightly ';
+    } else if (selectedPayPeriod === 'monthly') {
+        titlePrefix = 'My Monthly ';
+    }
+
+    incomeTitle.textContent = titlePrefix + 'Income';
+    budgetTitle.textContent = titlePrefix + 'Budget';
+    spendsTitle.textContent = titlePrefix + 'Spends';
+    resultsTitle.textContent = titlePrefix + 'Results';
+}
+
 // Update currency spans based on currency selected
-currency.addEventListener('change', function () {
-    let currencySpans = document.getElementsByClassName('currency-selected');
-    let selectedCurrency = currency.value;
+document.getElementById('currency').addEventListener('change', function () {
+    const currencySpans = document.getElementsByClassName('currency-selected');
+    const selectedCurrency = this.value;
 
     for (let i = 0; i < currencySpans.length; i++) {
         currencySpans[i].innerHTML = selectedCurrency;
